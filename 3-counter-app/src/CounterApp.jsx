c477b40042173fa441f586c924a86222e0bce55f
ArmandoEnrique1010K <enrique1010k@gmail.com>
2025-01-15 00:49:49 -0500

    03

--- 3-counter-app/src/CounterApp.jsx
+++ 3-counter-app/src/CounterApp.jsx
@@ -2,13 +2,26 @@ import { useState } from "react";
 
 export const CounterApp = ({ value }) => {
 
+    // Variable de estado (state) counter utilizando el hook useState, incluye una }
+    // función setCounter para actualizar el estado
+
+    // Su valor por defecto esta dado por la propiedad value
     const [counter, setCounter] = useState(value);
 
+    // Función para incrementar el contador, setCounter sirve para actualizar el 
+    // estado en base al argumento.
+
+    // Mediante una función de flecha, toma el valor actual de counter y lo 
+    // incrementa en 1
     const counterIncrement = () => setCounter(c => c + 1);
 
-
     return <>
+        {/* Muestra el valor actual del state counter */}
         <h2>El valor del contador es {counter}</h2>
+
+        {/* Recuerda que <button></button> sirve para añadir un botón, además se 
+        incluye un evento onClick que contiene un callback o función de flecha 
+        sin argumentos que se ejecuta cuando el usuario hace clic en él */}
         <button onClick={counterIncrement} >incrementar contador +1</button>
     </>
 }
--- 3-counter-app/src/main.jsx
+++ 3-counter-app/src/main.jsx
@@ -4,6 +4,7 @@ import { CounterApp } from './CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Pasa un valor en la propiedad value al componente CounterApp */}
     <CounterApp value={7} />
   </React.StrictMode>
 )
